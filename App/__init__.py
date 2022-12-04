be2f5ee4d618279a7226818228d0f24754d32846
Guilherme <guilhermepytech1408@gmail.com>
2022-12-03 22:31:18 -0300

    Resolvendo o problema do cadastro

---
+++ App/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db = SQLAlchemy()
+
+db.init_app(app)
+
+from App import default
---
+++ App/default.py
@@ -0,0 +1,26 @@
+from App import app, db
+from flask import render_template, request, redirect, url_for
+from App.tables import User
+
+@app.route("/users")
+def index():
+    usuarios = db.session.execute(db.select(User).order_by(User.username)).scalars()
+    return render_template("index.html", users=usuarios)
+    
+
+@app.route("/users/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        user = User(
+            username=request.form["nome"], 
+            email=request.form["email"])
+            
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return "Deu bom"
